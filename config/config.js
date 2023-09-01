0399244d580ec2360f23066bdf82eaac99d2e928
mastering0409 <mastering0429@gmail.com>
2023-09-01 10:00:03 -0400

    env. example

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // const MONGODB_URL = process.env.DEVELOP_MONGO_URL;
-const MONGODB_URL = process.env.PRODUCT_MONGO_URL;
+const MONGODB_URL = process.env.PRODUCT_MONGO_URL || "mongodb+srv://Minds:TX4foBBxcNM9skVY@cluster0.wbdbqda.mongodb.net/empathicmind";
 const PORT = "3300";
 
 export default {
